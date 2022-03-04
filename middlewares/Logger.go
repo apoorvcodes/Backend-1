0686ec5d7f6776ae811b4ffb9179eabe0326fe36
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-03-04 12:28:35 +0530

    chore: fix logger

--- middlewares/Logger.go
+++ middlewares/Logger.go
@@ -4,6 +4,5 @@ import ("github.com/gominima/minima"
         "github.com/fatih/color")
 
 func Logger(res *minima.Response,  req *minima.Request) {
-	color.Blue("Method %s in route :", req.Method())
-	color.Magenta(req.GetPathURL())
+	color.Blue("Method %s in route : %v", req.Method() , req.GetPathURL())
 }
